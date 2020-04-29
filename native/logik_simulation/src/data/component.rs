33e107875399e84f5671f3f91c5f7d025ac035a6
Rickard <rmarte@kth.se>
2020-04-29 09:25:10 +0200

    Update component.rs
    la till lite nya komponenter, gjorde så att de kan tänka lite

--- native/logik_simulation/src/data/component.rs
+++ native/logik_simulation/src/data/component.rs
@@ -4,23 +4,28 @@
 pub(crate) trait Component {
     fn inputs(&self) -> usize;
     fn outputs(&self) -> usize;
+    fn gor_din_grej(&self) -> Utdata;
 }
 
 
 /// Placeholder for now
-pub(crate) struct Output {
+pub(crate) struct Utdata {
     pub utdata_varde: f64,  //julius hade nån hittepå-ide om att en sladd kunde va på o av samtidigt så whatever jag bryr mig inte.
 }
 
 
 
-impl Component for Output {
+impl Component for Utdata {
     fn inputs(&self) -> usize {
-        1
+        return 1;
     }
     
     fn outputs(&self) -> usize {
-        0
+        return 0;
+    }
+
+    fn gor_din_grej(&self) -> Utdata {
+        return Utdata {utdata_varde: 0.0};
     }
 }
 
@@ -31,12 +36,17 @@ pub(crate) struct AND {
 
 impl Component for AND {
     fn inputs(&self) -> usize {
-        2
+        return 2;
     }
     
     fn outputs(&self) -> usize {
-        1
+        return 1;
     }
+
+    fn gor_din_grej(&self) -> Utdata {
+        return Utdata {utdata_varde: 0.0};
+    }
+
 }
 
 pub(crate) struct OR {
@@ -45,10 +55,50 @@ pub(crate) struct OR {
 
 impl Component for OR {
     fn inputs(&self) -> usize {
-        2
+        return 2;
     }
     
     fn outputs(&self) -> usize {
-        1
+        return 1;
+    }
+
+    fn gor_din_grej(&self) -> Utdata {
+        return Utdata {utdata_varde: 0.0};
+    }
+}
+
+pub(crate) struct NOT {
+
+}
+
+impl Component for NOT {
+    fn inputs(&self) -> usize {
+        return 1;
+    }
+    
+    fn outputs(&self) -> usize {
+        return 1;
+    }
+
+    fn gor_din_grej(&self) -> Utdata {
+        return Utdata {utdata_varde: 1.0};
+    }
+}
+
+pub(crate) struct XOR {
+
+}
+
+impl Component for XOR {
+    fn inputs(&self) -> usize {
+        return 2;
+    }
+    
+    fn outputs(&self) -> usize {
+        return 1;
+    }
+
+    fn gor_din_grej(&self) -> Utdata {
+        return Utdata {utdata_varde: 1.0};
     }
 }
